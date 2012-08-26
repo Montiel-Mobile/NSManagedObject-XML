ec1b08e2689838bff20a23ed2ba248237b9536d9
John Montiel <john@montiel.org>
2012-08-26 01:59:26 -0700

    Add DDXMLElement category

---
+++ DDXMLElement+conv.h
@@ -0,0 +1,14 @@
+//
+//  DDXMLElement+conv.h
+//  CPSStabilization
+//
+//  Created by John Montiel on 5/21/12.
+//  Copyright (c) 2012 Em Sevn, LLC. All rights reserved.
+//
+
+#import "DDXMLElement.h"
+
+@interface DDXMLElement (conv)
+- (NSString *)valueForTag:(NSString *)inTag;
+
+@end
